8a24ab204ee4ad67f02db37c3dcae567aa502a4d
Jhon-Acevedo <jhon.acevedo01@uptc.edu.co>
2023-05-14 15:19:53 -0500

    Add new powerup

--- Scripts/Nivel1/Nivel1.gd
+++ Scripts/Nivel1/Nivel1.gd
@@ -2,15 +2,16 @@ extends Node2D
 
 var width = 9000
 var hight = 1080
-#var amount_powerUp = 0
-var amount_powerUp_heart = 0
-var amount_powerUp_shot_triple = 0
 var location = Vector2()
 
+# Cantidad de powerUp en el nivel 1
+var amount_powerUp = 5
 
+# Instancias de los powerUps
 var powerup_heart = preload("res://Scenes/PowerUps/FullVida.tscn").instance()
-var powerup_shot_triple = preload("res://Scenes/PowerUp.tscn").instance()
+var powerup_shot_triple = preload("res://Scenes/PowerUps/PowerUp.tscn").instance()
 
+# Probabilidad de que cada powerUp aparezca
 var _powerups = {
 	"heart": {"Probability":0.2},
 	"shot_triple": {"Probability":0.5},
@@ -19,12 +20,23 @@ var _powerups = {
 
 func _ready():
 	randomize()
-	for i in range(25):
+	for i in range(amount_powerUp):
 		var random_num = randf()
 		if random_num <= _powerups.get("heart").get("Probability"):
 			create_powerUpHeart()
+		if random_num <= _powerups.get("shot_triple").get("Probability"):
+			create_powerUpShoot()
 			
 
+
+func create_powerUpShoot():
+	var powerup_heart = preload("res://Scenes/PowerUps/PowerUp.tscn").instance()
+	location.x = rand_range(2500,width)
+	location.y = rand_range(550,550)
+	powerup_heart.position = location
+	#print(location)
+	add_child(powerup_heart)
+	
 
 func create_powerUpHeart():
 	var powerup_heart = preload("res://Scenes/PowerUps/FullVida.tscn").instance()
